1921c96cefed530cd2b945369d6440653d9ec537
NewgateBR <33495647+n7lucas@users.noreply.github.com>
2023-04-21 06:44:12 +0100

    Update index.js

--- Module9_MovieSearchAPI/index.js
+++ Module9_MovieSearchAPI/index.js
@@ -69,11 +69,3 @@ document.getElementById("watchLink").addEventListener("click", (e) => {
 
 } )
 
-document.addEventListener("DOMContentLoaded", function() {
-    document.querySelectorAll('a').forEach(function(link) {
-      link.addEventListener('click', function(event) {
-        event.preventDefault();
-        history.pushState({}, null, link.getAttribute('href'));
-      });
-    });
-  });
